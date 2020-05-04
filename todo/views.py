65dc977bc9751e20faa4e524e04c1f91f031ef84
Pedro Cruz <pedro_cruz_1995@hotmail.com>
2020-05-04 22:03:19 +0100

    startapp and setup

---
+++ todo/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+
+def signupuser(request):
+
+    return render(request, 'todo/signupuser.html',
+                  {'form': UserCreationForm()})
